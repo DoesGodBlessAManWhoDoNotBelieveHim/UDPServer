0f086a2b2832fe64234c93a7c5dbe25839fda926
Jing Zhang <king91chang@163.com>
2015-07-29 16:27:04 +0800

    1
    1

---
+++ UDPServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UDPServer
+//
+//  Created by wrt on 15/7/29.
+//  Copyright (c) 2015年 wrtsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UDPServer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UDPServer
+//
+//  Created by wrt on 15/7/29.
+//  Copyright (c) 2015年 wrtsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
